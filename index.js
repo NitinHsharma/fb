5211b34f757bd7a80508423bf37cf1d6861662d8
NitinHsharma <nitin.sharma969@gmail.com>
2018-03-27 00:12:17 +0530

    pushed by script

--- index.js
+++ index.js
@@ -4,32 +4,41 @@ const FBBotFramework = require('fb-bot-framework');
 const data = require('./config.json');
 const bodyParser = require('body-parser')
 
-	console.log(data);
-	// Initialize
+console.log(data);
+// Initialize
 const bot = new FBBotFramework(data);
 
 // Setup Express middleware for /webhook
 app.use('/webhook', bot.middleware());
-		// parse application/json
-		app.use(bodyParser.json())
+// parse application/json
+app.use(bodyParser.json())
 
-		// Setup listener for incoming messages
-		bot.on('message', (userId, message) => {
-			bot.sendTextMessage(userId, 'Echo Message: ' + message);
-		});
+let usersId = [];
+// Setup listener for incoming messages
+bot.on('message', (userId, message) => {
+	usersId.push({
+		userid: userId
+	});
+	usersId.forEach(element => {
+		if (element.userId != undefined) {
+			bot.sendTextMessage(element.userId, 'Echo Message: ' + message);
+
+		}
+	})
+});
 
 
-		app.get('/', (req, res) => {
-			console.log('request came');
-			res.send('hello world');
-		});
+app.get('/', (req, res) => {
+	console.log('request came');
+	res.send('hello world');
+});
 
 
 
-		var server = app.listen(process.env.PORT || 5000, function() {
+var server = app.listen(process.env.PORT || 5000, function() {
 
-			var host = server.address().address
-			var port = server.address().port
+	var host = server.address().address
+	var port = server.address().port
 
-			console.log('Example app listening at http://%s:%s', host, port)
-		})
+	console.log('Example app listening at http://%s:%s', host, port)
+})
