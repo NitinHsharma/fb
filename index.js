f8545f82076f9071c85c1ed2e2eebf36395e0f7c
NitinHsharma <nitin.sharma969@gmail.com>
2018-03-26 23:46:40 +0530

    for heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bot = new FBBotFramework({
 });
 
 // Setup Express middleware for /webhook
-		app.use(‘/webhook’, bot.middleware());
+app.use(‘/webhook’, bot.middleware());
 		// parse application/json
 		app.use(bodyParser.json())
 
@@ -22,5 +22,10 @@ const bot = new FBBotFramework({
 			res.send(“hello world”);
 		});
 
-		//Make Express listening
-		app.listen(process.env.PORT || 3000);
+		var server = app.listen(process.env.PORT || 5000, function() {
+
+			var host = server.address().address
+			var port = server.address().port
+
+			console.log("Example app listening at http://%s:%s", host, port)
+		})
