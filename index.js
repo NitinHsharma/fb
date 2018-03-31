4fc9ddbf6e1b8568ed3bd6590e51c8c0b3595862
NitinHsharma <nitin.sharma969@gmail.com>
2018-03-31 09:52:19 +0530

    checking logs

--- index.js
+++ index.js
@@ -16,16 +16,21 @@ app.use(bodyParser.json())
 let usersId = [];
 // Setup listener for incoming messages
 bot.on('message', (userId, message) => {
+	console.log('msg came from 'userId, message);
 	usersId.push({
 		userid: userId
 	});
+
 	usersId.forEach(element => {
+		console.log('array is ', element);
 		if (element.userId != undefined) {
 			bot.sendTextMessage(element.userId, 'Echo Message: ' + message);
 
 		}
 	})
 });
+
+bot.on('')
 
 
 app.get('/', (req, res) => {
