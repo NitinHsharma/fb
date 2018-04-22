2ae937cd7e9e31e2d63974555a8cc59d552c7a0e
NitinHsharma <nitin.sharma969@gmail.com>
2018-04-22 19:00:44 +0530

    pushed by script

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
 const FBBotFramework = require('fb-bot-framework');
-const data = require('./config.json');
 const bodyParser = require('body-parser')
-
-console.log(data);
 // Initialize
-const bot = new FBBotFramework(data);
-
+const bot = new FBBotFramework({
+	"page_token": process.env.pageToken,
+	"verify_token": process.env.verifyToken
+});
 // Setup Express middleware for /webhook
 app.use('/webhook', bot.middleware());
 // parse application/json
 app.use(bodyParser.json())
-
 var users = [];
 // Setup listener for incoming messages
 bot.on('message', (userId, message) => {
-	console.log('msg came from ', userId, message);
 	if (message.indexOf('me:') == 0) {
 		let user = message.split(':');
 		users.push({
@@ -24,31 +21,17 @@ bot.on('message', (userId, message) => {
 			userId: userId
 		})
 	}
-	console.log('before ', users);
 	var sender = users.filter(x => x.userId == userId);
-	console.log('afetr ', sender[0]);
 	users.forEach(element => {
-		console.log('array is ', element);
 		if (element.userId != userId) {
 			bot.sendTextMessage(element.userId, sender[0].username + ' Said: ' + message);
-			console.log('msg sent to ', element.userId);
 		}
 	})
 });
-
-
-
 app.get('/', (req, res) => {
-	console.log('request came');
 	res.send('hello world');
 });
-
-
-
 var server = app.listen(process.env.PORT || 5000, function() {
-
 	var host = server.address().address
 	var port = server.address().port
-
-	console.log('Example app listening at http://%s:%s', host, port)
 })
